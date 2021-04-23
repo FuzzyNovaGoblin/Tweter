448b855a3a7aa70ed4b986553465fe2d0bee179b
liam.cannon <liam.cannon@mymail.champlain.edu>
2021-04-22 20:03:49 -0400

    Drawer

--- lib/Singleton.dart
+++ lib/Singleton.dart
@@ -1,11 +1,11 @@
-import 'package:flutter/cupertino.dart';
-
 class Singleton {
   static final Singleton _singletonRef = Singleton._internal();
   Singleton._internal();
 
   factory Singleton() => _singletonRef;
 
+  String user_name = "INSERT USERNAME";
 
   // int UID
 }
+
