7af1665fa8bb68dc56561fbc6514454ef6ce9b33
liamcannon <liam.cannon@mymail.champlain.edu>
2021-04-27 20:26:11 -0400

    button format

--- lib/Pages/ProfilePage.dart
+++ lib/Pages/ProfilePage.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:tweter/UX/MainDrawer.dart';
-import 'package:tweter/UX/ReTweet.dart';
 import 'package:tweter/UX/Titlebar.dart';
 import 'package:tweter/Singleton.dart';
 import 'package:tweter/UX/Tweet.dart';
@@ -20,76 +19,83 @@ class _ProfilePageState extends State<ProfilePage> {
       body: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Container(
-            constraints: BoxConstraints(maxWidth: 598),
-            alignment: Alignment.center,
-            // color: Theme.of(context).accentColor,
-            child: Card(
-              child: ListView(
-                children: [
-                  Container(
-                    alignment: Alignment.centerLeft,
-                    constraints: BoxConstraints(maxWidth: 100, maxHeight: 100),
-                    child: Image.asset('assets/bird.png'),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      // need to add padding
-                      Singleton().userName,
-                      textAlign: TextAlign.left,
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                      // insert follower-following counts
+          Flexible(
+            child: Container(
+              constraints: BoxConstraints(maxWidth: 598),
+              alignment: Alignment.center,
+              // color: Theme.of(context).accentColor,
+              child: Card(
+                child: ListView(
+                  children: [
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      constraints:
+                          BoxConstraints(maxWidth: 100, maxHeight: 100),
+                      child: Image.asset('assets/bird.png'),
                     ),
-                  ),
-                  Container(
-                    height: 20,
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                        border: Border(
-                            top:
-                                BorderSide(width: 2.0, color: Colors.white60))),
-                    child: Row(children: [
-                      Expanded(
-                        child: Container(
-                            decoration: BoxDecoration(
-                                border: Border(
-                                    right: BorderSide(
-                                        width: 1.0, color: Colors.white60))),
-                            child: InkWell(
-                              onTap: () {
-                                // need to add
-                              },
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Text(
-                              "Tweets",
-                              textAlign: TextAlign.center,
-                            ),
-                                ))),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        // need to add padding
+                        Singleton().userName,
+                        textAlign: TextAlign.left,
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                        // insert follower-following counts
                       ),
-                      Expanded(
-                        child: Container(
-                            decoration: BoxDecoration(
-                                border: Border(
-                                    left: BorderSide(
-                                        width: 1.0, color: Colors.white60))),
-                            child: InkWell(
-                              onTap: () {
-                                // need to add 
-                              },
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Text(
-                              "Retweets",
-                              textAlign: TextAlign.center,
-                            ),
-                                ))),
-                      ),
-                    ], mainAxisAlignment: MainAxisAlignment.spaceEvenly),
-                  ),
-                ],
+                    ),
+                    Container(
+                      height: 20,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                          border: Border(
+                              top: BorderSide(
+                                  width: 2.0, color: Colors.white60))),
+                      child: Row(children: [
+                        Expanded(
+                          child: Container(
+                              decoration: BoxDecoration(
+                                  border: Border(
+                                      right: BorderSide(
+                                          width: 1.0, color: Colors.white60))),
+                              child: InkWell(
+                                  onTap: () {
+                                    // need to add
+                                  },
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Text(
+                                      "Tweets",
+                                      textAlign: TextAlign.center,
+                                      style:
+                                          Theme.of(context).textTheme.headline5,
+                                    ),
+                                  ))),
+                        ),
+                        Expanded(
+                          child: Container(
+                              decoration: BoxDecoration(
+                                  border: Border(
+                                      left: BorderSide(
+                                          width: 1.0, color: Colors.white60))),
+                              child: InkWell(
+                                  onTap: () {
+                                    // need to add
+                                  },
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Text(
+                                      "Retweets",
+                                      textAlign: TextAlign.center,
+                                      style:
+                                          Theme.of(context).textTheme.headline5,
+                                    ),
+                                  ))),
+                        ),
+                      ], mainAxisAlignment: MainAxisAlignment.spaceEvenly),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
